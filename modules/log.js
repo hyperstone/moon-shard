39d79dd0e8bddcd80a5106537346381ab923856a
Florian Wendelborn <florianwendgithub@gmail.com>
2015-06-09 13:10:16 +0200

    fix log and install.sh, add react to bower

--- modules/log.js
+++ modules/log.js
@@ -1,12 +1,13 @@
 // require external
 var Log = require('compact-log');
+var path = require('path');
 
 // require internal
 var config = require('./config');
 
 // set up compact-log
 var log = new Log({
-	path: process.mainModule.__dirname + config.log.path,
+	path: path.join(__dirname, '..', config.log.path),
 	levelmode: config.log.levelmode
 });
 
